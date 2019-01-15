4de68e281787134b9ae6a822da4e3340efd7d914
mauricen <maurice@morninglight.co.uk>
2019-01-15 13:13:33 +0000

    Update Length.scala

--- src/main/scala/labanswers/lab1a/Length.scala
+++ src/main/scala/labanswers/lab1a/Length.scala
@@ -4,6 +4,9 @@ abstract class Length(value: Double) extends Ordered[Length] {
   val conversion: Double
   private[lab1a] def metre = Metre(value / conversion)
   def compare(that: Length): Int = metre.compare(that.metre)
+  def toCentimetres: Centimetre = Centimetre(value / conversion)
+  def toMetres: Metre = Metre(value / conversion)
+  def toKilometres: Kilometre = Kilometre(value / conversion)
 }
 
 case class Metre(value: Double) extends Length(value: Double) {
@@ -16,16 +19,12 @@ case class Centimetre(value: Double) extends Length(value: Double) {
   override val conversion: Double = 100
   def this(m: Metre) {this(m.value * 100)}
   def +(other: Length): Centimetre = new Centimetre(metre + other.metre)
-  def toMetres: Metre = Metre(value / conversion)
-  def toKilometres: Kilometre = Kilometre(value / conversion)
 }
 
 case class Kilometre(value: Double) extends Length(value: Double) {
   override val conversion = 0.001
   def this(m: Metre) {this(m.value * 0.001)}
   def +(other: Length): Kilometre = new Kilometre(metre + other.metre)
-  def toCentimetres: Centimetre = Centimetre(value / conversion)
-  def toMetres: Metre = Metre(value / conversion)
 }
 
 object Qn1a {
